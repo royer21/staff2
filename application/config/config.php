067570c20b80fad035108997cd120177a3a05024
Eliecer David Pari Alhuay <parialhuay@gmail.com>
2015-06-24 19:45:14 -0500

    configurango area local

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/staff/';
+$config['base_url']	= 'http://staff.muni/';
 
 /*
 |--------------------------------------------------------------------------
